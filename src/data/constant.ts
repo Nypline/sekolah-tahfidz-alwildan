568147ec26ce2fa678c706526a705fcc4cd431b6
Nypline <muhammaddhiyaizza@gmail.com>
2025-03-11 11:59:27 +0700

    add : item menu

---
+++ src/data/constant.ts
@@ -0,0 +1,23 @@
+export const menuItem = [
+  {
+    label: "Beranda",
+    href: "#hero",
+  },
+  {
+    label: "Tentang Kami    ",
+    href: "#about",
+  },
+  {
+    label: "Program",
+    href: "#service",
+  },
+  {
+    label: "Galeri",
+    href: "#gallery",
+  },
+
+  {
+    label: "Kontak Kami",
+    href: "#contact",
+  },
+];
