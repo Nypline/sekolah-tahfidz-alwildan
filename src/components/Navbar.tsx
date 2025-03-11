97d1147f2d5c68578c1fd572a6c40daf4a520d6e
Nypline <muhammaddhiyaizza@gmail.com>
2025-03-11 11:58:33 +0700

    feat: add navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,93 @@
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+import Link from "next/link";
+import { menuItem } from "@/data/constant";
+import { Button } from "@/components/ui/button";
+
+const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleMenuClick = () => {
+    setIsMenuOpen(false);
+  };
+
+  return (
+    <nav className="bg-white shadow-md fixed inset-x-0 z-20">
+      <div className="container mx-auto px-4 flex justify-between items-center py-5">
+        {/* Logo */}
+        <div className="flex items-center max-w-40">
+          <Link
+            href="/"
+            className="cursor-pointer"
+            onClick={(e) => {
+              e.preventDefault();
+              window.scrollTo({ top: 0, behavior: "smooth" });
+            }}
+          >
+            <Image src="./next.svg" alt="logo-nav" height={50} width={50} />
+          </Link>
+        </div>
+
+        {/* Desktop Menu */}
+        <ul className="hidden md:flex space-x-6 text-gray-700">
+          {menuItem.map((item, index) => (
+            <li key={index}>
+              <a href={item.href} className="hover:text-[#287C74]">
+                {item.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <Button variant="cta" className="hidden md:flex">
+          daftar sekarang
+        </Button>
+
+        {/* Hamburger Menu (Mobile) */}
+        <button
+          className="block md:hidden text-gray-700 focus:outline-none"
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        >
+          <div
+            className={`h-1 w-6 bg-gray-700 mb-1 transition-transform duration-300 ${
+              isMenuOpen ? "rotate-45 translate-y-2" : ""
+            }`}
+          ></div>
+          <div
+            className={`h-1 w-6 bg-gray-700 mb-1 transition-opacity duration-300 ${
+              isMenuOpen ? "opacity-0" : ""
+            }`}
+          ></div>
+          <div
+            className={`h-1 w-6 bg-gray-700 transition-transform duration-300 ${
+              isMenuOpen ? "-rotate-45 -translate-y-2" : ""
+            }`}
+          ></div>
+        </button>
+      </div>
+
+      {/* Mobile Menu */}
+      {isMenuOpen && (
+        <div className="md:hidden bg-white shadow-md">
+          <ul className="space-y-4 text-gray-700 p-4">
+            {menuItem.map((item, index) => (
+              <li key={index}>
+                <a
+                  href={item.href}
+                  className="block hover:text-orange-500"
+                  onClick={handleMenuClick}
+                >
+                  {item.label}
+                </a>
+              </li>
+            ))}
+            <Button variant="cta">daftar sekarang</Button>
+          </ul>
+        </div>
+      )}
+    </nav>
+  );
+};
+
+export default Navbar;
