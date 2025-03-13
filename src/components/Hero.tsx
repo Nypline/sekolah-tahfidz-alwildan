ab1b21c84e19d9950e669890ded89635b0299c6a
Nypline <muhammaddhiyaizza@gmail.com>
2025-03-13 11:45:44 +0700

    chrone : change main button, change main heading text

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,6 +3,7 @@ import { styles } from "./styles";
 import { rightHeroImage } from "@/assets";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import ExploreButton from "@/components/ui/ExploreButton";
 
 const Hero = () => {
   return (
@@ -10,11 +11,11 @@ const Hero = () => {
       className="bg-white flex justify-center items-center pt-20 md:pt-24 lg:pt-14 h-screen"
       id="hero"
     >
-      <div className="container mx-auto px-2 md:px-4 h-full grid grid-cols-1 lg:grid-cols-2 items-center gap-2 sm:gap-20 ">
+      <div className="px-16 py-10 xl:px-16 xl:py-10 lg:px-10 lg:py-7 md:px-6 md:py-5 sm:px-2 sm:py-1.5 mx-auto h-full grid grid-cols-1 lg:grid-cols-2 items-center gap-2 sm:gap-20 ">
         {/* Left Section */}
         <div className="text-center lg:text-left text-white">
           <h1 className={`${styles.heroHeadText} text-black`}>
-            lorem ipsum set dolor amet
+            Sekolah Inklusi Tahfizd AL-Wildan
           </h1>
 
           <p className={`${styles.paragrafText} pb-8 sm:pb-16 text-black`}>
@@ -24,12 +25,13 @@ const Hero = () => {
           </p>
           <div>
             <Link href="#contact" passHref>
-              <Button
+              {/* <Button
                 variant="mainbutton"
                 className="px-6 py-2  bg-[#287C74] text-white font-semibold shadow-md hover:bg-[#68b2ab] transition duration-500 text-sm md:text-base lg:text-lg hover:border-transparent rounded-lg h-14 hover:cursor-pointer"
               >
                 Yuk gabung dengan kami
-              </Button>
+              </Button> */}
+              <ExploreButton />
             </Link>
           </div>
         </div>
